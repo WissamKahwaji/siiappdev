cc4e5375be75e49d4995c48d7ee881dc822a1bbd
WissamKahwaji <kahwajiwisam@gmail.com>
2024-07-01 12:51:22 +0400

    edits

--- src/components/const/QrCodeUser.tsx
+++ src/components/const/QrCodeUser.tsx
@@ -33,29 +33,19 @@ const QrCodeUser = ({ qrCodeUrl, userName, qrCodeRef }: QrCodeUserProps) => {
       {qrCodeUrl ? (
         <div
           className="flex flex-col justify-center items-center w-52"
-          ref={qrCodeRef}
+
+          // onClick={handleSaveAsImage}
         >
-          <canvas
-            ref={canvasRef}
-            className="border-4 border-secondary shadow-sm shadow-secondary mb-2 w-full h-full"
-          />
+          <div className="w-full h-full p-2" ref={qrCodeRef}>
+            <img
+              src={qrCodeUrl}
+              alt="User QR Code"
+              className="w-full h-full border-4 border-secondary shadow-sm shadow-secondary mb-2"
+            />
+          </div>
           <p className="text-lg font-serif text-navBackground">{userName}</p>
         </div>
       ) : (
-        // <div
-        //   className="flex flex-col justify-center items-center w-52"
-
-        //   // onClick={handleSaveAsImage}
-        // >
-        //   <div className="w-full h-full p-2" ref={qrCodeRef}>
-        //     <img
-        //       src={qrCodeUrl}
-        //       alt="User QR Code"
-        //       className="w-full h-full border-4 border-secondary shadow-sm shadow-secondary mb-2"
-        //     />
-        //   </div>
-        //   <p className="text-lg font-serif text-navBackground">{userName}</p>
-        // </div>
         <p>Loading QR code...</p>
       )}
     </div>
